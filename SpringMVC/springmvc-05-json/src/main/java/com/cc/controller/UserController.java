16241ebc06d989dc96923f836b10f33eca1d2ab7
chengcheng1021 <chengcheng1021@aliyun.com>
2020-08-30 20:58:04 +0800

    jackson使用

---
+++ SpringMVC/springmvc-05-json/src/main/java/com/cc/controller/UserController.java
@@ -0,0 +1,55 @@
+package com.cc.controller;
+
+import com.cc.pojo.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @RequestMapping("/j1")
+    //@ResponseBody//它就不会走视图解析器，直接返回一个字符串
+    public String json1() throws JsonProcessingException {
+        User user = new User(100, "策划个昵称", 3);
+        System.out.println(user.toString());
+        ObjectMapper objectMapper = new ObjectMapper();
+        String s = objectMapper.writeValueAsString(user);
+        return s;
+    }
+
+    @RequestMapping("/j2")
+    //@ResponseBody//它就不会走视图解析器，直接返回一个字符串
+    public String json2() throws JsonProcessingException {
+        List<User> userList = new ArrayList<User>();
+
+        User user1 = new User(100, "策划个昵称", 3);
+        User user2 = new User(100, "策划个昵称", 3);
+        User user3 = new User(100, "策划个昵称", 3);
+
+        userList.add(user1);
+        userList.add(user2);
+        userList.add(user3);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String s = objectMapper.writeValueAsString(userList);
+        return s;
+    }
+
+    @RequestMapping("/j3")
+    //@ResponseBody//它就不会走视图解析器，直接返回一个字符串
+    public String json3() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Date date = new Date();
+
+        String s = objectMapper.writeValueAsString(date);
+        return s;
+    }
+}
---
+++ SpringMVC/springmvc-05-json/src/main/java/com/cc/filter/EncodingFilter.java
@@ -0,0 +1,20 @@
+package com.cc.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class EncodingFilter implements Filter {
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletRequest.setCharacterEncoding("UTF-8");
+        servletResponse.setCharacterEncoding("UTF-8");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    public void destroy() {
+
+    }
+}
